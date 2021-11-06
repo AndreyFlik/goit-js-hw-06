88231031de03b6bdc48a47d6a2e0c118c10e9523
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2021-11-06 23:12:38 +0200

    1

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,5 @@
 const catSum = categories.querySelectorAll(`.item`).length;
 console.log(`Number of categories: ${catSum}`);
+console.log(catText.h2);
+// const catText = catSum.querySelectorAll(`h2`);
+// console.log(catText);
