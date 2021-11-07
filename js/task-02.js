a8d12c98d05301ad98b5601861cb38d1f07c89c4
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2021-11-07 18:09:34 +0200

    часть 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const selector = document.querySelector("#ingredients");
+
+const ingredientsIter = ingredients.map((itm) => {
+  const listIng = document.createElement("li");
+  listIng.classList.add("item");
+  listIng.textContent = itm;
+  console.log(listIng);
+  selector.append(listIng);
+});
