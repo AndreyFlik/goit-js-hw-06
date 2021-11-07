383467a1198b14e1e061385805d54528045ffcbb
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2021-11-07 17:34:49 +0200

    final-t1

--- js/task-01.js
+++ js/task-01.js
@@ -3,8 +3,9 @@ console.log(`Number of categories: ${catSum.length}`);
 
 const arrayEl = [...catSum];
 
-const tittleName = arrayEl.map(
-  (tittle) => tittle.firstElementChild.textContent
-);
-console.log(`Category: ${tittleName}`);
-console.log(`tittleName`);
+const tittleName = arrayEl.map((tittle) => {
+  const nameH1 = tittle.firstElementChild.textContent;
+  console.log(`Category: ${nameH1}`);
+  const nameEL = tittle.querySelectorAll(`li`);
+  console.log(`Elements: ${nameEL.length}`);
+});
