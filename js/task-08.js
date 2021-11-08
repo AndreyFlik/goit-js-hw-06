c8f2c95d861bc7fd0403a2d96e2852f8849b78ef
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2021-11-08 18:06:29 +0200

    часть 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
+// const inputEl = formEl.querySelectorAll("input");
+// console.log(inputEl);
 
+formEl.addEventListener("submit", onformEl);
+
+function onformEl(event) {
+  event.preventDefault();
+  if (
+    "" === event.currentTarget.elements.password.value ||
+    event.currentTarget.elements.email.value === ""
+  ) {
+    return alert(`Ты что то забыл`);
+  }
+  const forEl = {
+    mail: event.currentTarget.elements.email.value,
+    password: event.currentTarget.elements.password.value,
+  };
+  console.log(forEl);
+  formEl.reset();
+}
