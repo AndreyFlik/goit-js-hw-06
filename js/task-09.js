6f7970e1f622cf056da7f56dee607767247168b0
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2021-11-08 19:58:55 +0200

    часть 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+const btn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+
+const handleClick = () => {
+  return (body.style.backgroundColor = `#${Math.floor(
+    Math.random() * 16777215
+  ).toString(16)}`);
+};
+
+btn.addEventListener("click", handleClick);
